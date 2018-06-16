90aaa307badf842f088c673a780b6267a3841e87
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-16 13:12:40 -0500

    define replaceVowels function

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var word = document.querySelector('input[name=word]');
 var button = document.querySelector('button');
+
+function replaceVowels () {
+  const regex = /a|e|i|o|u/gi;
+  var header = document.createElement('h1');
+  header.innerText = word.value.replace(regex, '');
+  document.body.insertBefore(header,word);
+}
