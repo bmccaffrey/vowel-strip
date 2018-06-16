267df90a3d7b71bcfc15e17f41c2932c6407532d
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-16 13:50:35 -0500

    declare and assign variable for input element's section

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var word = document.querySelector('input[name=word]');
+var word = document.querySelector('.word');
 var button = document.querySelector('button');
+var inputElements = document.querySelector('.input-elements');
 
 function replaceVowels () {
   const regex = /a|e|i|o|u/gi;
   var header = document.createElement('h1');
   header.innerText = word.value.replace(regex, '');
-  document.body.insertBefore(header,word);
+  document.body.insertBefore(header, inputElements);
 }
 
 button.addEventListener('click', replaceVowels);
